7d53501c14374d89dc5fa0d9a722d2f7f3aa71ff
Sharl Sherif <s.sherif@seamlabs.com>
2021-05-25 05:19:19 +0200

    Power & Subtraction.

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
-
+#include <stdio.h>
+#include <stdlib.h>
 enum Operations {
 	Subtraction = 1,
 	Addition = 2,
 	Multiplication = 3,
+	Division = 4,
+	Power = 5
 };
 
 
@@ -12,48 +15,106 @@ int Menu() {
 	cout << "Choose the operation:"<< endl;
 	cout << "1 - Subtraction"<< endl;
 	cout << "2 - Addition"<<endl;
-	cout << "3 - Multiplication"<<endl;
+	cout << "3 - Multiplication" << endl;
+	cout << "4 - Division" << endl;
+	cout << "5 - Power"<<endl;
 	
 	float choice = -1;
 	do {
 		cout << "Your choice:" << endl;
 		cin >> choice;
-	} while (choice != Subtraction && choice != Addition && choice != Multiplication);
+	} while (choice != Subtraction && choice != Power && choice != Division && choice != Addition && choice != Multiplication);
 
 	return choice;
 }
+int askHowManyNumbers() {
+	int result = 0;
+	cout << "How many numbers?"<<endl;
+	cin >> result;
+	return result;
+}
 
+float* parseInput(int n) {
+	float *numbers;
+	numbers = (float *) malloc(n);
+	for (int i = 0; i < n; i++) {
+		cin >> numbers[i];
+	}
+	return numbers;
+}
 
 class Calc {
 
-	void run(int Op) {
-		float x[2] = { 0.3, 0.1 };
-		float result;
-		switch (Op) {
-			case Addition:
-				result = this.addition(x, 2);
-				break;
-			case Multiplication:
-				result = this.multiplication(x, 2);
-				break;
+	public:
+		void run(int Op, float* numbers, int size) {
+			float result = -1.0;
+			switch (Op) {
+				case Addition:
+					result = addition(numbers, size);
+					break;
+				case Subtraction:
+					result = subtraction(numbers, size);
+					break;
+				case Multiplication:
+					result = multiplication(numbers, size);
+					break;
+				case Division:
+					result = division(numbers, size);
+					break;
+				case Power:
+					result = power(numbers);
+					break;
+			}
+			cout << result<< endl;
 		}
-		printf("%f", result);
-	}
 
-	float addition(float n[], int size) {
-		float sum=0;
-		for (int i = 0; i < size; i++) sum += n[i];
-		return sum;
-	}
-	float multiplication(float n[], int size) {
-		float mult = 0;
-		for (int i = 0; i < size; i++) mult += n[i];
-		return mult;
-	}
-};
+		float addition(float* n, int size = 2) {
+			float sum = 0.0;
+			for (int i = 0; i < size; i++) sum += n[i];
+			return sum;
+		}
+		float subtraction(float* n, int size = 2) {
+			float sub = n[0];
+			for (int i = 1; i < size; i++) sub -= n[i];
+			return sub;
+		}
+		float multiplication(float* n, int size=2) {
+			float mult = 1.0;
+			for (int i = 0; i < size; i++) mult = mult * n[i];
+			return mult;
+		}
+		float division(float* n, int size = 2) {
+			float div = 1.00;
+			for (int i = 0; i < size; i++) div = div/ n[i];
+			return div;
+		}
+
+		// we subtract 1 from the power because it already starts the first multiplication using the number given.
+		// so if number = 5, power = 2. it should equal 25 but instead it would be more because it multiplies 5 by itself 3 times
+		float power(float* n) {
+			float number = n[0];
+			float power = n[1];
+			float powerQuery[2] = { n[1], 1 };
+			float powerMinusOne = subtraction(powerQuery);
+
+			float result = number;
+			
+			for (int i = 0; i < powerMinusOne; i++) {
+				float multiplyNumberByItself[2] = { result, number };
+				result = multiplication(multiplyNumberByItself);
+			}
+			return result;
+		}
+
+		float root(float* n) {
+
+		}
+} calc;
 
 int main()
 {
 	float choice = Menu();
-	// test
+	int inputsize = askHowManyNumbers();
+	float* input = parseInput(inputsize);
+	calc.run(choice, input, inputsize);
 }
